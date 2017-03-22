08bec786db6e4f94c52cb79ad5a1c6b221564069
cloudchen <jchen@cloudits.info>
2017-03-22 11:41:40 +0800

    JS基本案例及元素操作

---
+++ src/main/html/myjs.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cloudchen on 2017-03-22.
+ */
+var i;
+document.write("<table>");
+for(i=1;i<10;i++)
+{
+    document.write("<tr><td>"+i+"</td></tr>");
+}
+document.write("</table>");
