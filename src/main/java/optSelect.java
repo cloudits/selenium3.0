d720bc6e212808e2a94666a2e7bc4957740e4701
cloudchen <jchen@cloudits.info>
2017-06-23 15:13:20 +0800

    使用xpath1.html和xpath2.html来演示对超链接及文本框的基本访问方法

---
+++ src/main/java/optSelect.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by cloudchen on 2017-06-23.
+ */
+public class optSelect {
+    static WebDriver driver;
+    public static void optlink()
+    {
+        driver.get("http://localhost/selenium3/xpath1.html");
+        WebElement element;
+        element=driver.findElement(By.xpath("/html/body/div/a"));//访问超链接
+        element.click();//点击超链接
+    }
+    public static void optinput()
+    {
+        driver.get("http://localhost/selenium3/xpath2.html");
+        WebElement element;
+        element=driver.findElement(By.xpath("//input"));//访问input文本框
+        element.sendKeys("cloud");//输入内容
+        element.clear();//清空文本框
+    }
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:\\Windows\\System32\\geckodriver.exe");
+        System.setProperty("webdriver.firefox.bin","C:\\Program Files (x86)\\Mozilla Firefox51\\firefox.exe");
+        driver=new FirefoxDriver();
+      //  optSelect.optlink();//访问超链接对象
+        optSelect.optinput();//访问文本框
+
+
+    }
+}
