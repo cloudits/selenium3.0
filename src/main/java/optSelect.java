6f79ac01df7727b5d6a5711c58757632d3793db7
cloudchen <jchen@cloudits.info>
2017-06-26 15:35:51 +0800

    添加对于button访问的代码

--- src/main/java/optSelect.java
+++ src/main/java/optSelect.java
@@ -22,13 +22,23 @@ public class optSelect {
         element=driver.findElement(By.xpath("//input"));//访问input文本框
         element.sendKeys("cloud");//输入内容
         element.clear();//清空文本框
+        element=driver.findElement(By.xpath("//input[2]"));//访问input文本框
+        element.sendKeys("cloud");//输入内容
+    }
+    public static void optbutton()
+    {
+        driver.get("http://localhost/selenium3/xpath2.html");
+        WebElement element;
+        element=driver.findElement(By.xpath("//input[3]"));//访问input按钮
+        element.click();
     }
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\Windows\\System32\\geckodriver.exe");
         System.setProperty("webdriver.firefox.bin","C:\\Program Files (x86)\\Mozilla Firefox51\\firefox.exe");
         driver=new FirefoxDriver();
       //  optSelect.optlink();//访问超链接对象
-        optSelect.optinput();//访问文本框
+       // optSelect.optinput();//访问文本框
+        optSelect.optbutton();//访问按钮
 
 
     }
