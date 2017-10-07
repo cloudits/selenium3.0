37cfa0807a384fde1c80003bf661c18fcd8687ad
cloudchen <458890418@qq.com>
2017-10-07 15:09:56 +0800

    添加comfirm,prompt,iframe的代码

--- src/main/java/ExtendObj.java
+++ src/main/java/ExtendObj.java
@@ -14,7 +14,32 @@ public class ExtendObj {
         System.setProperty("webdriver.firefox.bin","C:\\Program Files (x86)\\Mozilla Firefox51\\firefox.exe");
         driver=new FirefoxDriver();
         driver.navigate().to("http://127.0.0.1/selenium3/alerthtml.html");
-        alertopt();
+       // alertopt();
+       // promptopt();
+        iframeopt();
+    }
+    public static void iframeopt()
+    {
+        driver.navigate().to("http://127.0.0.1/selenium3/iframe.html");
+        driver.switchTo().frame("sidebar");
+        System.out.println(driver.findElement(By.xpath("html/body")).getText());
+        driver.switchTo().parentFrame();
+        driver.switchTo().frame(1);
+        System.out.println(driver.findElement(By.xpath("html/body")).getText());
+        driver.switchTo().defaultContent();
+
+    }
+    public static void promptopt()
+    {
+        driver.findElement(By.xpath("//button[3]")).click();
+        Alert alert=driver.switchTo().alert();//切换到弹出alert框上
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        alert.sendKeys("TestOps.cn");
+        alert.accept();//确认
     }
     public static void alertopt()
     {
